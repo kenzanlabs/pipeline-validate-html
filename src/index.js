11ce7d0b4d8904e2f72d2c86c1da9a1c12a1abed
Paul Barry <pbarry@kenzan.com>
2017-01-30 11:47:08 -0500

    Verify custom options are formatted for linting use

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@
 
 var htmllint = require('gulp-htmllint');
 var lazypipe = require('lazypipe');
+var path = require('path');
+var handyman = require('pipeline-handyman');
+var fs = require('fs');
+
+var DEFAULT_PATH = path.join(process.cwd(), 'node_modules/pipeline-validate-html/.htmllintrc');
+var DEFAULT_OPTIONS = {
+  rules: {},
+  config: DEFAULT_PATH,
+  plugins: [],
+  failOnError: false
+};
 
 module.exports = {
 
@@ -14,8 +25,38 @@ module.exports = {
 function pipelineFactory (options) {
   var stream;
 
+  var rules;
+
+  if (typeof options === 'object') {
+
+    rules = handyman.mergeConfig(retrieveDefaultOptions(), options);
+
+    options = handyman.mergeConfig(DEFAULT_OPTIONS, {
+      rules: rules,
+      config: null
+    });
+
+  } else {
+    options = DEFAULT_OPTIONS;
+
+  }
+
   stream = lazypipe()
     .pipe(htmllint, options);
 
   return stream();
+}
+
+function retrieveDefaultOptions () {
+  var defaultOptions = {};
+
+  try {
+    defaultOptions = JSON.parse(fs.readFileSync(DEFAULT_PATH, 'utf-8'));
+
+  } catch (ex) {
+    handyman.log('Could not retrieve default options from included config file at %s.', DEFAULT_PATH);
+
+  }
+
+  return defaultOptions;
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -7,8 +7,10 @@ var dirtyChai = require('dirty-chai');
 var isStream = require('isstream');
 var fs = require('fs');
 var path = require('path');
+var rimraf = require('rimraf');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var handyman = require('pipeline-handyman');
 
 var gutil = require('gulp-util');
 
@@ -20,6 +22,38 @@ chai.use(sinonChai);
 chai.use(dirtyChai);
 
 describe('pipeline-validate-html', function () {
+
+  var nodePath;
+
+  before(function (done) {
+    /*
+     To mimic an environment in which this pipeline will be used,
+     it's necessary to have a "node_modules/pipeline-validate-html/" directory with a .htmllintrc file.
+     To ensure this file exists, a simple stream is used to generate the file in
+     the appropriate location.
+     */
+
+    var localPath = path.join(process.cwd());
+
+    nodePath = path.join(process.cwd(), 'node_modules/pipeline-validate-html/');
+
+    if (!fs.existsSync(nodePath)) {
+      fs.mkdirSync(nodePath);
+    }
+
+    fs.createReadStream(localPath + '/.htmllintrc')
+      .pipe(fs.createWriteStream(nodePath + '/.htmllintrc'))
+      .on('finish', done);
+  });
+
+  after(function () {
+    /*
+     Delete temporary pipeline-validate-css directory after testing is complete.
+     */
+    if (fs.existsSync(nodePath)) {
+      rimraf.sync(nodePath);
+    }
+  });
 
   describe('validateHTML Method', function () {
 
@@ -38,8 +72,8 @@ describe('pipeline-validate-html', function () {
       it('should output an error via gulp-util when an invalid HTML file is found', function (done) {
 
         /*
-        gulp-htmllint uses gulp-util internally to output the error messages.
-        We stub the log method to A) verify it has been called; B) return an empty array to reduce output during testing
+         gulp-htmllint uses gulp-util internally to output the error messages.
+         We stub the log method to A) verify it has been called; B) return an empty array to reduce output during testing
          */
         var spy = sinon.stub(gutil, 'log').returns([]);
         var fileName = 'invalid-html.html';
@@ -77,6 +111,34 @@ describe('pipeline-validate-html', function () {
 
             done();
           });
+
+      });
+
+      describe('validateHTML provided a custom options object', function () {
+
+        var customConfig;
+
+        beforeEach(function () {
+          customConfig = {
+            'attr-name-style': 'dash'
+          };
+
+        });
+
+        it('should format the provided options to the default options structure', function () {
+          var spy = sinon.spy(handyman, 'mergeConfig');
+
+          validateHTMLPipeline.validateHTML(customConfig);
+
+          expect(spy).to.have.been.calledTwice();
+
+          expect(spy.getCall(0).args[0]['attr-name-style']).to.equal('lowercase');
+          expect(spy.getCall(0).args[1]).to.eql(customConfig);
+
+          expect(spy.getCall(1).args[1].rules['attr-name-style']).to.equal(customConfig['attr-name-style']);
+
+          handyman.mergeConfig.restore();
+        });
 
       });
 
